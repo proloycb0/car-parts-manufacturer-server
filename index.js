27b4054a1d45d92773c98b4feceac82f28d35327
Proloycb <proloychacroborty@gmail.com>
2022-09-03 15:07:18 +0600

    orders complete

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware of cors and express
 
@@ -17,8 +17,33 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-    } 
-    finally {}
+
+        const partsCollection = client.db('carnoCarUser').collection('parts');
+        const ordersCollection = client.db('carnoCarUser').collection('orders');
+
+        // parts api
+        app.get('/parts', async (req, res) => {
+            const result = await partsCollection.find().toArray();
+            const parts = result.reverse();
+            res.send(parts);
+        })
+
+        app.get('/parts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const part = await partsCollection.findOne(query);
+            res.send(part);
+        });
+
+        // orders api
+
+        app.post('/orders', async (req, res) => {
+            const orders = req.body;
+            const result = await ordersCollection.insertOne(orders);
+            res.send(result);
+        })
+    }
+    finally { }
 }
 
 run().catch(console.dir);
