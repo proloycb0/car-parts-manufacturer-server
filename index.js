339a4b61ccc4ae7af089d564e235fd6437482171
Proloycb <proloychacroborty@gmail.com>
2022-09-03 20:42:44 +0600

    orders delete by id

--- index.js
+++ index.js
@@ -14,12 +14,27 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o4ve3pa.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unAuthorized Access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 async function run() {
     try {
         await client.connect();
 
         const partsCollection = client.db('carnoCarUser').collection('parts');
         const ordersCollection = client.db('carnoCarUser').collection('orders');
+        const usersCollection = client.db('carnoCarUser').collection('users');
 
         // parts api
         app.get('/parts', async (req, res) => {
@@ -35,11 +50,43 @@ async function run() {
             res.send(part);
         });
 
+        // user api
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+            res.send({ result, token });
+        })
+
         // orders api
+
+        app.get('/order', verifyJWT, async (req, res) => {
+            const userEmail = req.query.userEmail;
+            const decodedEmail = req.decoded.email;
+            if(userEmail === decodedEmail){
+                const query = {userEmail: userEmail};
+                const orders = await ordersCollection.find(query).toArray();
+                res.send(orders);
+            }
+        });
 
         app.post('/orders', async (req, res) => {
             const orders = req.body;
             const result = await ordersCollection.insertOne(orders);
+            res.send(result);
+        });
+
+        app.delete('/orders/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id)};
+            const result = await ordersCollection.deleteOne(filter);
             res.send(result);
         })
     }
